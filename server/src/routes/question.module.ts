dded84426b3ba6ac9fc1f3474c119a379f3b8817
NoobCoder212 <dimaunahanisiahjordan@gmail.com>
2026-06-14 22:06:49 +0800

    Add update and delete questions

--- server/src/routes/question.module.ts
+++ server/src/routes/question.module.ts
@@ -1,8 +1,18 @@
-import express from 'express'
-import { createQuestion }from '../controllers/questionController.ts'
+import express from 'express' 
+import { 
+  createQuestion, 
+  fetchQuestion, 
+  fetchQuestions,
+  updateQuestion,
+  dropQuestion
+}from '../controllers/questionController.ts'
 
 const router = express.Router();
 
+router.get('/:id', fetchQuestion);
+router.put('/:id', updateQuestion);
+router.delete('/:id', dropQuestion);
+router.get('', fetchQuestions);
 router.post('/create', createQuestion);
 
 export default router
