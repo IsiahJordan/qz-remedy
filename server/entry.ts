4fcfaae14280fdba8ce68cfd834b2a76c2133900
NoobCoder212 <dimaunahanisiahjordan@gmail.com>
2026-06-14 16:28:23 +0800

    Fix log typo

--- server/entry.ts
+++ server/entry.ts
@@ -3,5 +3,5 @@ import api from './src/api.ts'
 const PORT = process.env.PORT || 3000;
 
 api.listen(PORT, '0.0.0.0', () => {
-  console.log(`Listening tp ${PORT}`);
+  console.log(`Listening to ${PORT}`);
 })
